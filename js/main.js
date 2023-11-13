f5ac7aadbdcc978866a338976c27b6c8a75502f6
2joopiter <veryberrylike3@gmail.com>
2023-11-13 11:51:22 +0900

    currenttarget/target 차이 + e.preventDefaul로 기본기능 막아주기

--- js/main.js
+++ js/main.js
@@ -14,10 +14,16 @@ const btns = main.querySelectorAll('li');
 const boxs = main.querySelectorAll('article');
 
 btns.forEach((btn, idx) => {
-	btn.addEventListener('click', () => {
-		//만약 클릭한 버튼에 on이 있다면 return으로 함수실행 중지
-		//만약 클릭한 버튼에 on이 없다면 해당 조건문이 무시되면서 activation 함수 호출
-		if (btns[idx].classList.contains('on')) return;
+	btn.addEventListener('click', (e) => {
+		console.log(e.currentTarget);
+		console.log(e.target);
+		// e.currentTarget: js 이벤트 구문에 연결되어있는 선택자가 선택 (코드상의 대상)
+		// e.target: 실제 화면상에서 이벤트가 발생한 대상이 선택됨 (보다 디테일함)
+
+		//만약 연결되어있는 선택자가 a요소의 링크 이동처럼 특정 기능이 있는 요소일시 해당 기능을 막아야 될 필요가 있음.
+		e.preventDefault(); // 브라우저 기본요소를 막는 코드
+
+		if (e.currentTarget.classList.contains('on')) return;
 		[btns, boxs].forEach((el) => activation(el, idx));
 	});
 });
