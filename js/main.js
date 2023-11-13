9f1986f13ee49340a246e5afa42d60c2e5dbb7a8
2joopiter <veryberrylike3@gmail.com>
2023-11-13 12:11:24 +0900

    innerText, innerHTML의 쓰임

--- js/main.js
+++ js/main.js
@@ -14,16 +14,20 @@ const btns = main.querySelectorAll('li');
 const boxs = main.querySelectorAll('article');
 
 btns.forEach((btn, idx) => {
-	btn.addEventListener('click', () => {
-		//직전에 모든 요소 초기화
-		btns.forEach((btn) => btn.classList.remove('on'));
-		// 클릭한 순번에 해당하는 버튼만 활성화
+	btn.addEventListener('click', (e) => {
+		//클릭시 해당 li의 글자 확인
+		console.log(e.currentTarget.innerText);
+
+		// 클릭한 li에 글자값을 순번으로 변경 (innerText객체의 key이기 때문에 대입연산자로 값 변경 가능)
+		// e.currentTarget.innerText = idx + 1;
+
+		// 클릭한 li의 글자값을 가져와서 <span>으로 감싼 다음에 li에 innerHTML로 등록
+		e.currentTarget.innerHTML = `<span>${e.currentTarget.innerText}</span>`;
 	});
 });
 
-boxs.forEach((box, idx) => {
-  btn.addEventListener('click', ()=>{
-    boxs.forEach((btn) => btn.classList.remove('on'));
-    boxs[idx].classList.add
-  })
-})
+console.dir(btns[0]);
+//DOM 안에서 자주 쓰이는 유용한 property
+//DOM.innerText : 해당 돔에 연결되어 있는 text값을 확인 가능
+//DOM.innerHTML : 해당 돔에 연결되어 있는 tag요소를 확인 가능
+//DOM.className : 해당 돔에 연결되어 있는 모든 클래스명을 문자열로 확인 가능
